7e48e12ce5d8152a62302fb1fd312ee8fab897c4
kenTrinh <hdk.trinh.2012@smu.edu.sg>
2016-01-25 13:22:39 +0800

    Display geojson data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,89 @@
 /*jslint browser: true*/
 /*global L */
 
-(function (window, document, L, undefined) {
-	'use strict';
 
-	L.Icon.Default.imagePath = 'images/';
+L.Icon.Default.imagePath = 'images/';
 
-	/* create leaflet map */
-	var map = L.map('map', {
-		center: [1.31, 103.8],
-		zoom: 11
-	});
+/* create leaflet map */
+var map = L.map('map', {
+    center: [1.31, 103.8],
+    zoom: 11
+});
 
-	/* add default stamen tile layer */
-	new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		minZoom: 0,
-		maxZoom: 18,
-		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
-	}).addTo(map);
+var buildings = new L.geoJson().addTo(map);
 
 
-	L.marker([52.5, 13.4]).addTo(map);
 
-}(window, document, L));
+var district_boundary = new L.geoJson();
+district_boundary.addTo(map);
+
+// $.get("/geojson", function(data, status){
+//         district_boundary.addData(data);
+//     }).error(function(err) {console.log(err)});
+
+
+
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [
+        [-100, 40],
+        [-105, 45],
+        [-110, 55]
+    ]
+}, {
+    "type": "LineString",
+    "coordinates": [
+        [-105, 40],
+        [-110, 45],
+        [-115, 55]
+    ]
+}];
+
+var myLayer = L.geoJson().addTo(map);
+myLayer.addData(myLines);
+/* add default stamen tile layer */
+new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 0,
+    maxZoom: 18,
+    attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+}).addTo(map);
+
+
+
+
+
+
+function handleFeature(feature, layer) {
+    layer.on({
+        mouseover: mouseoverfunction,
+        mouseout: mouseoutfunction
+    });
+}
+
+function mouseoverfunction(e) {
+    var properties = e.target.feature.properties;
+    for (var prop in properties) {
+        if (properties.hasOwnProperty(prop)) {
+            // or if (Object.prototype.hasOwnProperty.call(obj,prop)) for safety...
+            console.log("prop: " + prop + " value: " + properties[prop]);
+            document.getElementById("properties").innerHTML = document.getElementById("properties").innerHTML + "<tr><td>" + prop + "</td><td>" + properties[prop] + "</td></tr>";
+        }
+    }
+}
+
+function mouseoutfunction(e) {
+    document.getElementById("properties").innerHTML = "";
+}
+
+
+
+$.get("/geojson", function(data, status) {
+    L.geoJson(data, {
+        onEachFeature: handleFeature
+    }).addTo(map);
+
+    // console.log(data);
+
+}).error(function(err) {
+    console.log(err)
+});
